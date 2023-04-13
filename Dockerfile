f1a15da53e0beec31bb89c5cb15ad218a12069d3
Tim Basten <tbasten@gmail.com>
2023-04-13 22:23:59 +0800

    Fix static file. Issue existing once deployed. Now using whitenoise

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-FROM nginx:latest
-COPY /static/css/ /usr/share/nginx/html/static
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
-
 FROM python:3
 WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 COPY .env /app/.env
 COPY . .
+RUN python3 manage.py collectstatic --noinput
 CMD [ "python3", "manage.py", "runserver", "0.0.0.0:8000" ]
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
@@ -117,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
